465ca6873bc7843db6c99fc21ace240d8e768c01
Cody Polland <cody.polland@gmail.com>
2022-04-04 21:58:48 -0700

    Testing java scrpit with html

---
+++ script.js
@@ -0,0 +1,9 @@
+//initate start
+var startQuiz = document.getElementById('start-btn')
+var nextBtn = document.getElementById('next-btn')
+var questionContainerEl = document.getElementById('question-container')
+var questionsEl = document.getElementById('question')
+var countDown = document.getElementById('count')
+
+console.log(nextBtn)
+
