39bcbb1d8e164e68769ce102c30545d8bdc84d3b
Cody Polland <cody.polland@gmail.com>
2022-07-28 13:50:49 -0700

    Final Redo of Project

---
+++ JS/questions.js
@@ -0,0 +1,37 @@
+var questions = [
+  {
+    title: "What function lets you click on a button?",
+    choices: [
+      "addEventListener",
+      "addActionListener",
+      "addRunListener",
+      "addBtnListener",
+    ],
+    answer: "addEventListener",
+  },
+  {
+    title: "What is it called when you move around the DOM?",
+    choices: [
+      "DOM Travel",
+      "DOM Traversal",
+      "Window Traveling",
+      "DOM navigation",
+    ],
+    answer: "DOM Traversal",
+  },
+  {
+    title: "what array methods adds elements to the end of an array?",
+    choices: [".sort", ".slice", ".replace", ".push"],
+    answer: ".push",
+  },
+  {
+    title: "What does the && operator check for?",
+    choices: ["true true", "true false", "false false", "None of the above"],
+    answer: "true true",
+  },
+  {
+    title: "What lets you store data to a users computer?",
+    choices: ["external database", "MYSQL", "MongoDB", "Local Storage"],
+    answer: "Local Storage",
+  },
+];
